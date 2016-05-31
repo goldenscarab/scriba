4a3e942f4a312c89894b983fd86675f5c58316f4
Scarabdore <sylvain.carre@goldenscarab.fr>
2016-05-31 21:42:07 +0200

    Panneau liste - Alignement à droite des élements de droite

--- resources/views/back/partials/notes-list.blade.php
+++ resources/views/back/partials/notes-list.blade.php
@@ -4,11 +4,11 @@
 			<div class="item-wrapper row" data-id="{{ $n->id }}">
 				<div class="item-line col-xs-12">
 					<div class="note-name col-xs-7">{{ str_limit($n->name, config('scriba.note-list-len-max')) }}</div>
-					<div class="note-date col-xs-5">{{ $n->updated_at->diffForHumans() }}</div>
+					<div class="note-date col-xs-5 text-right">{{ $n->updated_at->diffForHumans() }}</div>
 				</div>
 				<div class="item-line col-xs-12">
 					<div class="note-subject col-xs-7">{{ str_limit(($n->subject != "")?$n->subject:$n->content, 30) }}</div>
-					<div class="note-size col-xs-5">{{ number_format(strlen($n->content), 0, ',', ' ')." Octets" }}</div>
+					<div class="note-size col-xs-5 text-right">{{ number_format(strlen($n->content), 0, ',', ' ')." Octets" }}</div>
 				</div>
 			</div>
 		@empty
